412626fba204fd3d54b0d1ae24e14592fc709242
qwertybee <rinradee.sae@gmail.com>
2021-06-28 22:05:33 +0700

    Patch up logic everywhere

--- src/main/java/zork/item/Gun.java
+++ src/main/java/zork/item/Gun.java
@@ -10,8 +10,8 @@ public class Gun extends Weapon implements Serializable {
         super.damage = -40;
         super.usage = "attack with";
         super.type = "weapon";
-        super.description = "A handy snub-nose revolver with "+ getRounds() +" rounds available. " +
-                "Use wisely! - there will be no ammos lying around. " +
+        super.description = "A handy snub-nose revolver with "+ getRounds() +" rounds available.\n" +
+                "Use wisely! - there will be no ammos lying around.\n" +
                 "Deals an average of -20 in damage to foe";
     }
 }
